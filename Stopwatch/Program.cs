17bd0a255c5632ea1c1f594b8d21fd6c33093ad6
Olivio Neto <netoolivio34@gmail.com>
2023-07-26 08:29:20 -0300

    Cronômetro Curso Balta

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length-1,1));
+            int time = int.Parse(data.Substring(0,data.Length-1));    
+
+
+            Console.WriteLine(data);
+            Console.WriteLine(type);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+            while(currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("StopWatch finalizado");
+            Thread.Sleep(2500);
+        }
+    }
+}
